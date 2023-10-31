abed0d36bf5c9db77f7aa155603618fbcc0f35a9
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-10-31 16:01:19 +0530

    update pages

---
+++ pages/api/convert.js
@@ -0,0 +1,88 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "fs";
+import formidable from "formidable";
+
+import { Writable } from "stream";
+const formidableConfig = {
+  keepExtensions: true,
+  maxFileSize: 10_000_000,
+  maxFieldsSize: 10_000_000,
+  maxFields: 10,
+  allowEmptyFiles: true,
+  multiples: true,
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+function formidablePromise(req, opts) {
+  return new Promise((accept, reject) => {
+    const form = formidable(opts);
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      return accept({ fields, files });
+    });
+  });
+}
+
+const fileConsumer = (acc) => {
+  const writable = new Writable({
+    write: (chunk, _enc, next) => {
+      acc.push(chunk);
+      next();
+    },
+  });
+
+  return writable;
+};
+
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+export default async function convert(req, res) {
+  try {
+    const chunks = [];
+
+    const { fields, files } = await formidablePromise(req, {
+      ...formidableConfig,
+      // consume this, otherwise formidable tries to save the file to disk
+      fileWriteStreamHandler: () => fileConsumer(chunks),
+    });
+
+    const { type } = fields;
+
+    if (!files.file) {
+      return res.status(400).json({ lol: "No file uploaded." });
+    }
+
+    // Determine the output file extension based on the 'type'
+    const outputExtension = type === "word" ? "docx" : "pdf";
+
+    // Generate a unique filename using a timestamp and random string
+    const timestamp = new Date().getTime();
+    const randomString = generateRandomString(8);
+    const uniqueFilename = `${timestamp}-${randomString}.${outputExtension}`;
+
+    // Define the output path where you want to save the converted file
+    const outputPath = `../../public/conversions/${uniqueFilename}`;
+
+    fs.renameSync(files.file.path, outputPath);
+    res.json({ filename: uniqueFilename });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred during conversion and save." });
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,173 +1,242 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [file, setFile] = useState(null);
+  const [convertedFile, setConvertedFile] = useState(null);
+  const [type, setType] = useState("word"); // Default to PDF
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleConvert = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("type", type);
+
+    try {
+      const response = await fetch("/api/convert", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("Conversion successful");
+      } else {
+        console.error("Conversion failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <>
-    <div className="hero" id="home">
-      <nav>
-        <h2 className="logo">
-          Convert<span>ify</span>
-        </h2>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about-text-id">About</a>
-          </li>
-          <li>
-            <a href="#more_conversions">More Conversions</a>
-          </li>
-          <li>
-            <a href="#contacts">Contact</a>
-          </li>
-        </ul>
-        <a href="#" className="btn">
-          Donate <i className="fa-solid fa-heart" />
-        </a>
-      </nav>
-      <div className="content">
-        <h4>Hey there! </h4>
-        <h1>
-          {" "}
-          Convert Word to <span>PDF</span>
-        </h1>
-        <h3>Upload your file right here</h3>
-        <input type="file" className='bg-blue-300 p-5 border border-blue-300 hover:bg-transparent' id="upload-file" >
-          Choose File
-          </input>
-        {/* <button  id="upload_button" className="upload-button">
-          Choose File
-        </button> */}
-        <span id="custom-text">No file chosen, yet.</span>
-        <style
-          dangerouslySetInnerHTML={{
-            __html:
-              "\n            #custom-text{\n                color:white;\n            }\n        "
-          }}
-        />
-      </div>
-      <img src="/images/feature image.png" className="feature-image" />
-    </div>
-    {/*About Section*/}
-    <section className="about" id="about-text-id">
-      <div className="main">
-        <img src="/images/about_us.png" alt="" className="about_us" />
-        <div className="about-text">
-          <h2>About Us</h2>
-          <h5>
-            Designers &amp; <span>Developers</span>
-          </h5>
-          <p>
+      <div className="hero" id="home">
+        <nav>
+          <h2 className="logo">
+            Convert<span>ify</span>
+          </h2>
+          <ul>
+            <li>
+              <Link href="#home">Home</Link>
+            </li>
+            <li>
+              <Link href="#about-text-id">About</Link>
+            </li>
+            <li>
+              <Link href="#more_conversions">More Conversions</Link>
+            </li>
+            <li>
+              <Link href="#contacts">Contact</Link>
+            </li>
+          </ul>
+          <Link href="/" className="btn">
+            Donate <i className="fa-solid fa-heart" />
+          </Link>
+        </nav>
+        <div className="content">
+          <h4>Hey there! </h4>
+          <h1>
             {" "}
-            We have begun our adventure into developing responsive and innovative
-            websites.
-          </p>
-          <form>
-            <a className="button-three" href="https://linktr.ee/josephjohnphilip">
-              Let's Talk
-            </a>
-          </form>
-        </div>
-      </div>
-    </section>
-    {/*Education*/}
-    <div className="service">
-      <div className="title" id="more_conversions">
-        <h2>More Conversions</h2>
-      </div>
-      <div className="box">
-        <div className="card">
-          <i className="fa-solid fa-file-pdf" />
-          <h5> Convert Word to PDF</h5>
-          <div className="para">
-            <p>
-              Convert Word files to PDF for free and quickly using an efficient
-              and dependable algorithm.{" "}
-            </p>
-            <p>
-              <br />
-              <br />
-              <br />
-            </p>
-            <p style={{ textAlign: "center" }}>
-              <a className="button" href="/index.html">
-                Click Here
+            <div>
+              {type === "pdf" && <span>Convert PDF to Word</span>}
+              {type === "word" && <span>Convert Word to PDF</span>}
+              {type === "pptx" && <span>Convert PPTX to PDF</span>}
+            </div>
+          </h1>
+          <h3>Upload your file right here</h3>
+          <input
+            type="file"
+            className="file:bg-blue-700 file:rounded-md file:p-3 file:border file:border-blue-300 file:hover:bg-transparent"
+            id="upload-file"
+            accept=".pdf, .docx, .pptx"
+            onChange={handleFileChange}
+          />
+
+          <button
+            id="upload_button"
+            className="p-2 rounded-sm bg-blue-900"
+            onClick={handleConvert}
+          >
+            Convert
+          </button>
+          {convertedFile && (
+            <div>
+              <a
+                href={convertedFile}
+                download={`converted.${type === "word" ? "docx" : "pdf"}`}
+              >
+                Download Converted File
               </a>
+            </div>
+          )}
+          <span id="custom-text">No file chosen, yet.</span>
+          <style
+            dangerouslySetInnerHTML={{
+              __html:
+                "\n            #custom-text{\n                color:white;\n            }\n        ",
+            }}
+          />
+        </div>
+        <img src="/images/feature image.png" className="feature-image" />
+      </div>
+      {/*About Section*/}
+      <section className="about" id="about-text-id">
+        <div className="main">
+          <img src="/images/about_us.png" alt="" className="about_us" />
+          <div className="about-text">
+            <h2>About Us</h2>
+            <h5>
+              Designers &amp; <span>Developers</span>
+            </h5>
+            <p>
+              {" "}
+              We have begun our adventure into developing responsive and
+              innovative websites.
             </p>
+            <form>
+              <a
+                className="button-three"
+                href="https://linktr.ee/josephjohnphilip"
+              >
+                Let's Talk
+              </a>
+            </form>
           </div>
         </div>
-        <div className="card">
-          <i className="fa-solid fa-file-word" />
-          <h5> Convert PDF to Word </h5>
-          <div className="para">
-            <p>
-              Convert Word files to PDF for free and quickly using an efficient
-              and dependable algorithm.
-            </p>
-            <p>
-              <br />
-              <br />
-              <br />
-            </p>
-            <p style={{ textAlign: "center" }}>
-              <a className="button" href="/pdftoword.html">
-                Click Here
-              </a>
-            </p>
-          </div>
+      </section>
+      {/*Education*/}
+      <div className="service">
+        <div className="title" id="more_conversions">
+          <h2>More Conversions</h2>
         </div>
-        <div className="card">
-          <i className="fa-solid fa-file-lines" />
-          <h5> Convert PPTX to PDF</h5>
-          <div className="para">
-            <p>
-              Convert PPTX files to PDF for free and quickly using an efficient
-              and dependable algorithm.
-            </p>
-            <p>
-              <br />
-              <br />
-              <br />
-            </p>
-            <p style={{ textAlign: "center" }}>
-              <a className="button" href="/pptxtopdf.html">
-                Click Here
-              </a>
-            </p>
+        <div className="box">
+          <div className="card">
+            <i className="fa-solid fa-file-pdf" />
+            <h5> Convert Word to PDF</h5>
+            <div className="para">
+              <p>
+                Convert Word files to PDF for free and quickly using an
+                efficient and dependable algorithm.{" "}
+              </p>
+              <p>
+                <br />
+                <br />
+                <br />
+              </p>
+              <div style={{ textAlign: "center" }}>
+                <div
+                  onClick={() => {
+                    setType("word");
+                  }}
+                  className="button"
+                >
+                  Click Here
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <i className="fa-solid fa-file-word" />
+            <h5> Convert PDF to Word </h5>
+            <div className="para">
+              <p>
+                Convert Word files to PDF for free and quickly using an
+                efficient and dependable algorithm.
+              </p>
+              <p>
+                <br />
+                <br />
+                <br />
+              </p>
+              <div style={{ textAlign: "center" }}>
+                <div
+                  onClick={() => {
+                    setType("pdf");
+                  }}
+                  className="button"
+                >
+                  Click Here
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <i className="fa-solid fa-file-lines" />
+            <h5> Convert PPTX to PDF</h5>
+            <div className="para">
+              <p>
+                Convert PPTX files to PDF for free and quickly using an
+                efficient and dependable algorithm.
+              </p>
+              <p>
+                <br />
+                <br />
+                <br />
+              </p>
+              <div style={{ textAlign: "center" }}>
+                <div
+                  onClick={() => {
+                    setType("pptx");
+                  }}
+                  className="button"
+                >
+                  Click Here
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    {/*Contact Me*/}
-    <div className="contact">
-      <p>Find Us On</p>
-      <a className="button-two" href="#">
-        {" "}
-        Follow us! It's Free!{" "}
-      </a>
-    </div>
-    {/*footter start*/}
-    <footer>
-      <p>Team Convertify</p>
-      <p> To find about more about us! Click this Links below</p>
-      <div className="social" id="contacts">
-        <a href="#">
-          <i className="fa-brands fa-instagram" />
-        </a>
-        <a href="#">
-          <i className="fa-brands fa-square-facebook" />
-        </a>
-        <a href="#">
-          <i className="fa-brands fa-linkedin" />
+      {/*Contact Me*/}
+      <div className="contact">
+        <p>Find Us On</p>
+        <a className="button-two" href="#">
+          {" "}
+          Follow us! It's Free!{" "}
         </a>
       </div>
-      <p className="end"> CopyRight by Team Convertify</p>
-    </footer>
-  </>
-  
-  )
+      {/*footter start*/}
+      <footer>
+        <p>Team Convertify</p>
+        <p> To find about more about us! Click this Links below</p>
+        <div className="social" id="contacts">
+          <Link href="/">
+            <i className="fa-brands fa-instagram" />
+          </Link>
+          <Link href="/">
+            <i className="fa-brands fa-square-facebook" />
+          </Link>
+          <Link href="/">
+            <i className="fa-brands fa-linkedin" />
+          </Link>
+        </div>
+        <p className="end"> CopyRight by Team Convertify</p>
+      </footer>
+    </>
+  );
 }
